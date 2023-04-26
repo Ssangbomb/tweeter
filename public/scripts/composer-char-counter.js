9a46510dbf0c89ff357410c207953e454a27658b
ssangbomb <ssangbomb@gmail.com>
2023-04-25 19:13:09 -0700

    hard coding tweeter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  const textArea =  $(area);
+  textArea.on('keypress', function(){
+    // console.log(this.value.length);
+    let num = 140 - this.value.length;
+    const counter = $('.counter').html(num);
+    console.log(num); 
+    if(num < 0) {
+      $('.counter').addClass('negative');
+    }
+  })
+});
